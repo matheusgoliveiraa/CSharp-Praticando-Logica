58c961e67baf909283c233d283dc5a7287ed2fdf
Matheus Oliveira <matheus.goliveira87@hotmail.com>
2025-08-05 15:50:28 -0300

    add paste
    Adicionando a pasta " Conceitos Básicos ".

---
+++ ConceitosBasicos/01-CalcularMediaNotas/CalcularMediaNotas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CalcularMediaNotas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a primeira nota: ");
+            float nota1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota: ");
+            float nota2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a última nota: ");
+            float nota3 = float.Parse(Console.ReadLine());
+
+            float soma = (nota1 + nota2 + nota3) / 3;
+
+            Console.WriteLine($"A média final é: {soma:F2}");
+
+        }
+    }
+}
