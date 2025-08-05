b55f7cba22ebd5f3e75f2c9fd4d6d608456b0ef6
Matheus Oliveira <matheus.goliveira87@hotmail.com>
2025-08-05 16:52:41 -0300

    Add files via upload
    Adicionando mais um exercício feito, cálculo de Celsius para Fahrenheit.

---
+++ 02-CelsiusFahrenheit/CelsiusFahrenheit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CelsiusFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a temperatura em graus Cº: ");
+            int celsius = int.Parse(Console.ReadLine());
+
+            double temperatura = celsius * 1.8;
+            float Fahrenheit = (float)(temperatura + 32);
+
+            Console.WriteLine("A temperatura de graus celsius para Fahrenheit é: " + Fahrenheit);
+        }
+    }
+}
