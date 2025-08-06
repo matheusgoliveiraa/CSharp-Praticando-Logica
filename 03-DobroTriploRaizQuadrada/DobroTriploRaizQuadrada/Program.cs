0cb4b7958400ed640988e22af0476e32bd491050
Matheus Oliveira <matheus.goliveira87@hotmail.com>
2025-08-06 11:44:33 -0300

    Add files
    Adicionando um exercício, que calcula o dobro e triplo de um número e também descobre sua raíz quadrada.

---
+++ 03-DobroTriploRaizQuadrada/DobroTriploRaizQuadrada/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite um número: ");
+int numero1 = int.Parse(Console.ReadLine());
+
+double dobro = numero1 * 2;
+double triplo = numero1 * 3;
+
+Console.WriteLine($"Dobro: {dobro}");
+Console.WriteLine($"Triplo: {triplo}");
+Console.WriteLine($"Raiz quadrada: {Math.Sqrt(numero1):F2}");
+
+
