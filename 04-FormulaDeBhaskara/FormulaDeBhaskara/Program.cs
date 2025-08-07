a2a06187adf1f55aea6be2939a278d7edd76ec21
Matheus Oliveira <matheus.goliveira87@hotmail.com>
2025-08-07 16:31:18 -0300

    Add files
    Programa simples, desenvolvido para utilizar a fórmula de Bhaskara.

---
+++ 04-FormulaDeBhaskara/FormulaDeBhaskara/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+double a, b, c, delta, r1, r2;
+
+a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+delta = Math.Pow(b, 2.0) - 4 * a * c;
+
+if (a == 0 || delta < 0.0)
+{
+    Console.WriteLine("Impossivel calcular");
+}
+else
+{
+    {
+        r1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+        r2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+        Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+        Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
+    }
+
+    Console.ReadLine();
+}
