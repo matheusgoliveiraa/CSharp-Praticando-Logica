a764894c2a04583de9da0a8baf991ea06cf6d6c9
Matheus Oliveira <matheus.goliveira87@hotmail.com>
2025-08-11 13:29:23 -0300

    Add files
    Adicionando programa, para ler dois números do usuário.

---
+++ 05-LerDoisNumeros/LerDoisNumeros/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LerDoisNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro número: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número: ");
+            int numero2 = int.Parse(Console.ReadLine());
+
+            int soma = numero1 + numero2;
+
+            Console.WriteLine("A soma é: " + soma);
+            
+        }
+    }
+}
