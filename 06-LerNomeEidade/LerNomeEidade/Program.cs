64b284d07b709fb07caaa0f38e934acf5f3f5ed5
Matheus Oliveira <matheus.goliveira87@hotmail.com>
2025-08-13 13:32:45 -0300

    Add files
    Adicionando um programa, para ler o nome e a idade de um usuário.

---
+++ 06-LerNomeEidade/LerNomeEidade/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LerNomeEidade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Olá, {nome}, você tem {idade} anos.");
+
+            Console.ReadLine();
+        }
+    }
+}
